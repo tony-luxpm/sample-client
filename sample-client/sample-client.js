561cab616692107faaa898a309acdee1471e0d2a
tony-luxpm <anthony.chua@luxpmsoft.com>
2021-08-23 09:29:57 +0900

    latest push

--- sample-client/sample-client.js
+++ sample-client/sample-client.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
 const port = 5000;
+app.use("/", router);
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json);
+
+
 require('../eureka-helper/eureka-helper').registerWithEureka('sample-client', port)
 console.log(`sample client service listening on port ${port}`);
 
-app.get('/', function(req,res) {
-    res.send("sample client app")
+router.get('/hello', (req,res) => {
+    res.status(200).send("sample client app")
+});
+
+router.post('/hello/post', (req, res) => {
+    res.status(201).send(`hello ${req.body}`)
 })
 
-app.listen(port);
+app.listen(port, ()=> {
+    console.log(`Started on port ${port}`);
+});
 
